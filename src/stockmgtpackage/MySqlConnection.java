4e0427795a38990f9dd1e4a263cd34e5add6c753
Dee <Dee>
2020-01-05 17:21:20 +0100

    This is my first commit

---
+++ src/stockmgtpackage/MySqlConnection.java
@@ -0,0 +1,26 @@
+package stockmgtpackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+public class MySqlConnection {
+
+    public static Connection getConnection(){
+    Connection con = null;
+    String url= "jdbc:mysql://localhost:3306/stockmgt_db?useLegacyDatetimeCode=false&serverTimezone=UTC";
+    String username= "root";
+    String password = "";
+       
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection(url, username, password);
+    }
+    catch(Exception e){
+        JOptionPane.showMessageDialog(null, e);
+    }
+    
+return con;    
+    }
+}
